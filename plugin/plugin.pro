sailfishos/libqofono

commit 85d01f047194a4ad9068b8136d472d81f99d40da

Add no-module-prefix option
Allow building module without prefix similar to: https://github.com/sailfishos/libconnman-qt/commit/625beb2b0d2ace4c59ce4e1dee5d2634640537d8

Signed-off-by: Herman van Hazendonk <github.com@herrie.org>

plugin/plugin.pro
@@ -21,15 +21,33 @@ HEADERS = \
 
 INCLUDEPATH += ../src
 
+contains(CONFIG,no-module-prefix) {
+    system("sed -i 's/@@ModulePrefix@@//' qmldir")
+    system("sed -i 's/@@ModulePrefix@@//' plugins.qmltypes")
+    MODULENAME = QOfono
+} else {
+    system("sed -i 's/@@ModulePrefix@@/MeeGo\./' qmldir")
+    system("sed -i 's/@@ModulePrefix@@/MeeGo\./' plugins.qmltypes")
+    MODULENAME = MeeGo/QOfono
+}
+
 OTHER_FILES += \
     plugin.json plugins.qmltypes qmldir
 
-qmldir.path = $$[QT_INSTALL_QML]/MeeGo/QOfono
-target.path = $$[QT_INSTALL_QML]/MeeGo/QOfono
+TARGETPATH = $$[QT_INSTALL_QML]/$$MODULENAME
 
 qmldir.files += qmldir plugins.qmltypes
+qmldir.path = $$TARGETPATH
+target.path = $$TARGETPATH
 
 INSTALLS += target qmldir
 
-qmltypes.commands = qmlplugindump -nonrelocatable MeeGo.QOfono 0.2 > $$PWD/plugins.qmltypes
+qmltypes.target = qmltypes
+
+contains(CONFIG,no-module-prefix) {
+    qmltypes.commands = qmlplugindump -nonrelocatable QOfono 0.2 > $$PWD/plugins.qmltypes
+} else {
+    qmltypes.commands = qmlplugindump -nonrelocatable MeeGo.QOfono 0.2 > $$PWD/plugins.qmltypes
+}
+
 QMAKE_EXTRA_TARGETS += qmltypes

plugin/plugins.qmltypes
@@ -4,14 +4,14 @@ import QtQuick.tooling 1.2
 // It is used for QML tooling purposes only.
 //
 // This file was auto-generated by:
-// 'qmlplugindump -nonrelocatable MeeGo.QOfono 0.2'
+// 'qmlplugindump -nonrelocatable @@ModulePrefix@@QOfono 0.2'
 
 Module {
     dependencies: ["QtQuick 2.0"]
     Component {
         name: "QOfonoAssistedSatelliteNavigation"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoAssistedSatelliteNavigation 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoAssistedSatelliteNavigation 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Signal {
@@ -34,7 +34,7 @@ Module {
     Component {
         name: "QOfonoCallBarring"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoCallBarring 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoCallBarring 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "voiceIncoming"; type: "string"; isReadonly: true }
         Property { name: "voiceOutgoing"; type: "string"; isReadonly: true }
@@ -102,7 +102,7 @@ Module {
     Component {
         name: "QOfonoCallForwarding"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoCallForwarding 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoCallForwarding 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "voiceUnconditional"; type: "string" }
         Property { name: "voiceBusy"; type: "string" }
@@ -163,7 +163,7 @@ Module {
     Component {
         name: "QOfonoCallMeter"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoCallMeter 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoCallMeter 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "Error"
@@ -204,7 +204,7 @@ Module {
     Component {
         name: "QOfonoCallSettings"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoCallSettings 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoCallSettings 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "hideCallerId"; type: "string" }
         Property { name: "voiceCallWaiting"; type: "string" }
@@ -259,7 +259,7 @@ Module {
     Component {
         name: "QOfonoCellBroadcast"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoCellBroadcast 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoCellBroadcast 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "enabled"; type: "bool" }
         Property { name: "topics"; type: "string" }
@@ -285,7 +285,7 @@ Module {
     Component {
         name: "QOfonoConnectionContext"
         prototype: "QOfonoObject"
-        exports: ["MeeGo.QOfono/OfonoContextConnection 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoContextConnection 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "active"; type: "bool" }
         Property { name: "provisioning"; type: "bool"; isReadonly: true }
@@ -373,7 +373,7 @@ Module {
     Component {
         name: "QOfonoConnectionManager"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoConnMan 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoConnMan 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "attached"; type: "bool"; isReadonly: true }
         Property { name: "bearer"; type: "string"; isReadonly: true }
@@ -431,7 +431,7 @@ Module {
     Component {
         name: "QOfonoHandsfree"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoHandsfree 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoHandsfree 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Property { name: "features"; type: "QStringList"; isReadonly: true }
@@ -455,7 +455,7 @@ Module {
     Component {
         name: "QOfonoHandsfreeAudioAgent"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoHandsfreeAudioAgent 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoHandsfreeAudioAgent 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "agentPath"; type: "string" }
         Signal {
@@ -469,7 +469,7 @@ Module {
     Component {
         name: "QOfonoHandsfreeAudioCard"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoHandsfreeAudioCard 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoHandsfreeAudioCard 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "Error"
@@ -500,7 +500,7 @@ Module {
     Component {
         name: "QOfonoHandsfreeAudioManager"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoHandsfreeAudioManager 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoHandsfreeAudioManager 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "CodecType"
@@ -539,7 +539,7 @@ Module {
     Component {
         name: "QOfonoLocationReporting"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoLocationReporting 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoLocationReporting 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Property { name: "type"; type: "string"; isReadonly: true }
@@ -554,7 +554,7 @@ Module {
     Component {
         name: "QOfonoManager"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoManager 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoManager 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modems"; type: "QStringList"; isReadonly: true }
         Property { name: "defaultModem"; type: "string"; isReadonly: true }
@@ -583,7 +583,7 @@ Module {
     Component {
         name: "QOfonoMessage"
         prototype: "QOfonoObject"
-        exports: ["MeeGo.QOfono/OfonoMessage 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoMessage 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "messagePath"; type: "string" }
         Property { name: "state"; type: "string"; isReadonly: true }
@@ -600,7 +600,7 @@ Module {
     Component {
         name: "QOfonoMessageManager"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoMessageManager 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoMessageManager 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "serviceCenterAddress"; type: "string" }
         Property { name: "useDeliveryReports"; type: "bool" }
@@ -672,7 +672,7 @@ Module {
     Component {
         name: "QOfonoMessageWaiting"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoMessageWaiting 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoMessageWaiting 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "voicemailWaiting"; type: "bool"; isReadonly: true }
         Property { name: "voicemailMessageCount"; type: "int"; isReadonly: true }
@@ -698,7 +698,7 @@ Module {
     Component {
         name: "QOfonoModem"
         prototype: "QOfonoObject"
-        exports: ["MeeGo.QOfono/OfonoModem 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoModem 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "powered"; type: "bool" }
         Property { name: "online"; type: "bool" }
@@ -798,7 +798,7 @@ Module {
     Component {
         name: "QOfonoNetworkOperator"
         prototype: "QOfonoObject"
-        exports: ["MeeGo.QOfono/OfonoNetworkOperator 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoNetworkOperator 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "Error"
@@ -862,7 +862,7 @@ Module {
     Component {
         name: "QOfonoNetworkOperatorListModel"
         prototype: "QAbstractListModel"
-        exports: ["MeeGo.QOfono/OfonoNetworkOperatorListModel 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoNetworkOperatorListModel 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Property { name: "valid"; type: "bool"; isReadonly: true }
@@ -883,7 +883,7 @@ Module {
     Component {
         name: "QOfonoNetworkRegistration"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoNetworkRegistration 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoNetworkRegistration 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "scanning"; type: "bool"; isReadonly: true }
         Property { name: "mode"; type: "string"; isReadonly: true }
@@ -981,7 +981,7 @@ Module {
     Component {
         name: "QOfonoPhonebook"
         prototype: "QOfonoModemInterface2"
-        exports: ["MeeGo.QOfono/OfonoPhonebook 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoPhonebook 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "importing"; type: "bool"; isReadonly: true }
         Signal {
@@ -994,7 +994,7 @@ Module {
     Component {
         name: "QOfonoPositioningRequestAgent"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoPositioningRequestAgent 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoPositioningRequestAgent 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "agentPath"; type: "string" }
         Signal {
@@ -1011,7 +1011,7 @@ Module {
     Component {
         name: "QOfonoRadioSettings"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoRadioSettings 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoRadioSettings 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Property { name: "technologyPreference"; type: "string" }
@@ -1047,7 +1047,7 @@ Module {
     Component {
         name: "QOfonoSimListModel"
         prototype: "QAbstractListModel"
-        exports: ["MeeGo.QOfono/OfonoSimListModel 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoSimListModel 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "valid"; type: "bool"; isReadonly: true }
         Property { name: "count"; type: "int"; isReadonly: true }
@@ -1064,7 +1064,7 @@ Module {
     Component {
         name: "QOfonoSimManager"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoSimManager 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoSimManager 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "Error"
@@ -1263,7 +1263,7 @@ Module {
     Component {
         name: "QOfonoSmartMessaging"
         prototype: "QOfonoModemInterface2"
-        exports: ["MeeGo.QOfono/OfonoSmartMessaging 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoSmartMessaging 0.2"]
         exportMetaObjectRevisions: [0]
         Signal {
             name: "registered"
@@ -1307,7 +1307,7 @@ Module {
     Component {
         name: "QOfonoSmartMessagingAgent"
         prototype: "QObject"
-        exports: ["MeeGo.QOfono/OfonoSmartMessagingAgent 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoSmartMessagingAgent 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "agentPath"; type: "string" }
         Signal {
@@ -1329,7 +1329,7 @@ Module {
     Component {
         name: "QOfonoSupplementaryServices"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoSupplementaryServices 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoSupplementaryServices 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "state"; type: "string"; isReadonly: true }
         Signal {
@@ -1412,7 +1412,7 @@ Module {
     Component {
         name: "QOfonoTextTelephony"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoTextTelephony 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoTextTelephony 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "modemPath"; type: "string" }
         Property { name: "ttyEnabled"; type: "bool" }
@@ -1428,7 +1428,7 @@ Module {
     Component {
         name: "QOfonoVoiceCall"
         prototype: "QOfonoObject"
-        exports: ["MeeGo.QOfono/OfonoVoiceCall 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoVoiceCall 0.2"]
         exportMetaObjectRevisions: [0]
         Enum {
             name: "Error"
@@ -1530,7 +1530,7 @@ Module {
     Component {
         name: "QOfonoVoiceCallManager"
         prototype: "QOfonoModemInterface"
-        exports: ["MeeGo.QOfono/OfonoVoiceCallManager 0.2"]
+        exports: ["@@ModulePrefix@@QOfono/OfonoVoiceCallManager 0.2"]
         exportMetaObjectRevisions: [0]
         Property { name: "emergencyNumbers"; type: "QStringList"; isReadonly: true }
         Property { name: "errorMessage"; type: "string"; isReadonly: true }

ofonotest/ofonotest.pro
@@ -3,6 +3,12 @@ QT += qml quick
 
 SOURCES += main.cpp
 
+contains(CONFIG,no-module-prefix) {
+    system("sed -i 's/@@ModulePrefix@@//' qml/ofonotest/main.qml")
+} else {
+    system("sed -i 's/@@ModulePrefix@@/MeeGo\./' qml/ofonotest/main.qml")
+}
+
 qml.files += \
     qml/ofonotest/main.qml
 qml.path = /opt/examples/libqofono-qt5/qml/ofonotest/main.qml

ofonotest/qml/ofonotest/main.qml
@@ -1,5 +1,5 @@
 import QtQuick 2.0
-import MeeGo.QOfono 0.2
+import @@ModulePrefix@@QOfono 0.2
 
 
 Rectangle {

plugin/qmldir
@@ -1,3 +1,3 @@
-module MeeGo.QOfono
+module @@ModulePrefix@@QOfono
 plugin QOfonoQtDeclarative
 typeinfo plugins.qmltypes

plugin/qofonodeclarativeplugin.cpp
@@ -66,7 +66,7 @@
 
 void QOfonoDeclarativePlugin::registerTypes(const char *uri, int major, int minor)
 {
-    Q_ASSERT(QLatin1String(uri) == QLatin1String("MeeGo.QOfono"));
+    Q_ASSERT(QLatin1String(uri) == QLatin1String("MeeGo.QOfono") || QLatin1String(uri) == QLatin1String("QOfono"));
 
     qmlRegisterType<QOfonoManager>(uri,major,minor,"OfonoManager");
     qmlRegisterType<QOfonoModem>(uri,major,minor,"OfonoModem");

commit 45f47da1701d12c7398044641f8e136833aa04b6

[libqofono] Update plugins.qmltypes. JB#59899
The change to the 'qmltypes' target with commit
85d01f047194a4ad9068b8136d472d81f99d40da (Add no-module-prefix option)
broke the semi-automated tests with the help of sfdk-qmltypes. Fixing
that just a bit to make it pass the tests for 4.5.0. In the master
branch the issue is gone.

plugin/plugin.pro
@@ -47,7 +47,8 @@ qmltypes.target = qmltypes
 contains(CONFIG,no-module-prefix) {
     qmltypes.commands = qmlplugindump -nonrelocatable QOfono 0.2 > $$PWD/plugins.qmltypes
 } else {
-    qmltypes.commands = qmlplugindump -nonrelocatable MeeGo.QOfono 0.2 > $$PWD/plugins.qmltypes
+    qmltypes.commands = qmlplugindump -nonrelocatable MeeGo.QOfono 0.2 \
+        |sed 's/MeeGo\.QOfono/@@ModulePrefix@@QOfono/' > $$PWD/plugins.qmltypes
 }
 
 QMAKE_EXTRA_TARGETS += qmltypes

plugin/plugins.qmltypes
@@ -427,6 +427,7 @@ Module {
             name: "removeContext"
             Parameter { name: "path"; type: "string" }
         }
+        Method { name: "resetContexts" }
     }
     Component {
         name: "QOfonoHandsfree"
@@ -535,6 +536,25 @@ Module {
             Parameter { name: "path"; type: "string" }
             Parameter { name: "codecs"; type: "CodecTypeFlags" }
         }
+    }
+    Component {
+        name: "QOfonoIpMultimediaSystem"
+        prototype: "QOfonoModemInterface"
+        exports: ["@@ModulePrefix@@QOfono/OfonoIpMultimediaSystem 0.2"]
+        exportMetaObjectRevisions: [0]
+        Enum {
+            name: "Registration"
+            values: {
+                "RegistrationUnknown": 0,
+                "RegistrationDisabled": 1,
+                "RegistrationEnabled": 2,
+                "RegistrationAuto": 3
+            }
+        }
+        Property { name: "registration"; type: "Registration" }
+        Property { name: "registered"; type: "bool" }
+        Property { name: "voiceCapable"; type: "bool"; isReadonly: true }
+        Property { name: "smsCapable"; type: "bool"; isReadonly: true }
     }
     Component {
         name: "QOfonoLocationReporting"