74b22c3d3fc4f715df9b89e1ca3e4680044a1c4e
Andres Salomon <dilinger@queued.net>
2026-04-27 19:58:59 +0000

    test: Set up local phonesim test environment using dbus-test-runner
    This is completely isolated from the host system's dbus, so there's no
    longer any need to run these tests as root or deal with connman/ofonod
    conflicts.
    
    This allows both running 'ctest' from the build directory (without a
    GUI, intended to be automated), or installing a libqofono-tests
    package and running tests manually via tests.xml (with a GUI).

--- test/auto/tests/runtest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-systemctl --user restart phonesim
-sleep 15
-exec /usr/sbin/run-blts-root /bin/su $USER -g sailfish-radio \
-        -c "/opt/tests/libqofono-qt5/${1?Test executable expected}"
